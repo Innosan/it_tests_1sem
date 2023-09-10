54f60e02f45370fd0eb814615d8c61502f789d3e
InnoSan <reau_beua@gmail.com>
2023-09-10 13:13:32 +0300

    test 2 task 1

--- test2_task1.c
+++ test2_task1.c
@@ -1,45 +1,37 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "math.h"
 #include "math_utils.h"
 
-double memo[1000];
-
-double solveReccurenceWithArray(int l) {
+double solveRecurrence(int l) {
     if (l == 1) {
         return 1.5;
-    } else if (memo[l] != 0) {
-        return memo[l]; // Return the precomputed value if available
-    } else if (l > 2) {
-        double f = 0.;
-
-        f = 2.5 * sin(solveReccurenceWithArray(l - 1) - solveReccurenceWithArray(l -2)) + 3 * cos(solveReccurenceWithArray(l - 1) + solveReccurenceWithArray(l - 2));
-        memo[l] = f;
-
-        return f;
-    } else return 2.;
-}
-
-double solveReccurenceWithPrevs(int l) {
-    static double prev1 = 1.5; // Variable to store the previous value
-    static double prev2 = 2.0;
-
-    if (l == 1) {
-        return prev1;
     } else if (l == 2) {
-        return prev2; // Return the precomputed value if available
+        return 2.0;
     } else {
-        double f = 0.;
-        f = 2.5 * sin(solveReccurenceWithPrevs(l - 1) - solveReccurenceWithPrevs(l -2)) + 3 * cos(solveReccurenceWithPrevs(l - 1) + solveReccurenceWithPrevs(l - 2));
-        prev2 = prev1;
-        prev1 = f;
-        return f;
+        double prev1 = 2.0;
+        double prev2 = 1.5;
+
+        double current;
+
+        double sum = prev1 + prev2;
+        for (int i = 3; i <= l; i++) {
+            current = 2.5 * sin(prev1 - prev2) + 3 * cos(prev1 + prev2);
+            sum += current;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return sum;
     }
 }
 
 int main(void) {
-    int n = 15;
+    int n = 150;
 
     getWelcomeMessage(getStudent());
+    double sum = solveRecurrence(n);
 
-    printf("Fibonacci of %d is %f\n", n, solveReccurenceWithArray(n));
+    printf("Sum of the recurrence from 1 to %d is %f\n", n, sum);
+
+    return EXIT_SUCCESS;
 }
